392fa9a99f22f8540da8ede853368425c5d08cfa
Adhoc123 <emonhasan1010@gmail.com>
2022-10-27 13:13:16 +0600

    added login system

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -12,6 +12,23 @@ const Login = () => {
     const googleProvider = new GoogleAuthProvider();
     const navigate = useNavigate();
 
+    const handleSubmit = event =>{
+      event.preventDefault();
+      const form = event.target;
+      const email = form.email.value;
+      const password = form.password.value;
+      console.log(email, password)
+      signIn(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user)
+        form.reset();
+        navigate('/')
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }
     const handleGoogleSignIn = () =>{
         providerLogin(googleProvider)
         .then((result) => {
@@ -22,28 +39,12 @@ const Login = () => {
             console.error(error)
         });
     }
-    const handleOnSubmit = event =>{
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password)
-        signIn(email, password)
-        .then((result) => {
-          const user = result.user;
-          console.log(user)
-          form.reset();
-          navigate('/')
-        })
-        .catch((error) => {
-          console.error(error)
-        });
-    }
+    
     return (
-      <Form onSubmit={handleOnSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control name="name" type="email" placeholder="Enter email" required/>
+          <Form.Control name="email" type="email" placeholder="Enter email" required/>
           
         </Form.Group>
   
@@ -51,22 +52,20 @@ const Login = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control name="password" type="password" placeholder="Password" required/>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        
         <Button variant="primary" type="submit">
           Login
         </Button>
         <Form.Text className="text-danger ms-3">
             We'll never share your email with anyone else.
         </Form.Text>
-        <ButtonGroup aria-label="Basic example" className='d-block mt-2 mx-auto'>
-            <Button onClick={handleGoogleSignIn} variant="outline-primary" className='me-4'><FaGoogle/></Button>
-            <Button variant="outline-danger"><FaGithub/></Button>
-        </ButtonGroup>
+        
         
       </Form>
-
+      //   <ButtonGroup aria-label="Basic example" className='d-block mt-2 mx-auto'>
+      //   <Button onClick={handleGoogleSignIn} variant="outline-primary" className='me-4'><FaGoogle/></Button>
+      //   <Button variant="outline-danger"><FaGithub/></Button>
+      //  </ButtonGroup>
       
     );
 };
