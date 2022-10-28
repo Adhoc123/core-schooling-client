ebe02306cea222ee930b7c879aa7db52447ce19a
Adhoc123 <emonhasan1010@gmail.com>
2022-10-28 18:46:04 +0600

    added pdf button

--- src/Pages/Shared/Topic/Topic.js
+++ src/Pages/Shared/Topic/Topic.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import ReactToPrint from 'react-to-print';
+import { useRef } from 'react';
 
 const Topic = () => {
+    const ref = useRef();
     const data = useLoaderData();
     return (
         <div className='single p-4 rounded'>
             <div className='d-flex'>
-                <div>
+                <div ref={ref}>
                 <img style={{height:'150px',borderRadius:'10px', width:'250px'}} src={data.picture}/>
                 <br></br>
                 <strong className='fs-3'>{data.title}</strong>
@@ -17,8 +20,10 @@ const Topic = () => {
                 </Button>
                 </div>
                 <div>
-                    <Button variant="outline-info" className='fs-5'><Link to='/contents' style={{ textDecoration: 'none' }}>DownLoad PDF</Link></Button>
-                    
+                    <ReactToPrint trigger={
+                        () => <Button variant="outline-info" className='fs-5'><Link to='/contents' style={{ textDecoration: 'none' }}>DownLoad PDF</Link></Button>
+                    } content={() => ref.current}/>
+                {/* <Button variant="outline-info" className='fs-5'><Link to='/contents' style={{ textDecoration: 'none' }}>DownLoad PDF</Link></Button> */}
                 </div>
             </div>
            
