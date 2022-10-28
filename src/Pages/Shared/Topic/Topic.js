0b74b4a92a686a4a5cfcb47a9f0d8f3bd2a7ed1f
Adhoc123 <emonhasan1010@gmail.com>
2022-10-28 19:18:34 +0600

    added pdf opition

--- src/Pages/Shared/Topic/Topic.js
+++ src/Pages/Shared/Topic/Topic.js
@@ -2,28 +2,32 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import ReactToPrint from 'react-to-print';
+import ReactToPdf from 'react-to-pdf';
 import { useRef } from 'react';
 
 const Topic = () => {
-    const ref = useRef();
+    const ref = React.createRef();
     const data = useLoaderData();
     return (
         <div className='single p-4 rounded'>
             <div className='d-flex'>
-                <div ref={ref}>
-                <img style={{height:'150px',borderRadius:'10px', width:'250px'}} src={data.picture}/>
-                <br></br>
-                <strong className='fs-3'>{data.title}</strong>
-                <p>{data.about}</p>
-                <Button variant="outline-info" className='fs-5'>
-                <Link to={`/checkout/${data.id}`} style={{ textDecoration: 'none' }}>Get premium access.</Link>
-                </Button>
+                <div >
+                    <div ref={ref}>
+                    <img style={{height:'150px',borderRadius:'10px', width:'250px'}} src={data.picture}/>
+                    <br></br>
+                    <strong className='fs-3'>{data.title}</strong>
+                    <p>{data.about}</p>
+                    </div>
+                    
+                    <Button variant="outline-info" className='fs-5'>
+                    <Link to={`/checkout/${data.id}`} style={{ textDecoration: 'none' }}>Get premium access.</Link>
+                    </Button>
                 </div>
                 <div>
-                    <ReactToPrint trigger={
-                        () => <Button variant="outline-info" className='fs-5'><Link to='/contents' style={{ textDecoration: 'none' }}>DownLoad PDF</Link></Button>
-                    } content={() => ref.current}/>
-                {/* <Button variant="outline-info" className='fs-5'><Link to='/contents' style={{ textDecoration: 'none' }}>DownLoad PDF</Link></Button> */}
+                  
+                <ReactToPdf targetRef={ref} filename="About.pdf">
+                   {({ toPdf }) => <Button variant="outline-info" className='fs-5' onClick={toPdf} style={{ textDecoration: 'none' }}>DownLoad PDF</Button>}
+               </ReactToPdf>
                 </div>
             </div>
            
