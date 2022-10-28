ddd4fa8a1d6b33069d393f73a307588e0bebd7ed
Adhoc123 <emonhasan1010@gmail.com>
2022-10-28 11:44:11 +0600

    enabled environment variable

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,15 @@
 import React, { createContext, useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import {AuthContext} from '../../contexts/AuthProvider/AuthProvider';
+import Spinner from 'react-bootstrap/Spinner';
 
 const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext)
+    const {user, loading} = useContext(AuthContext)
     const location = useLocation();
-
+    
+    if(loading){
+        return <Spinner animation="border" variant="warning" />;
+    }
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace/>;
     }
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCv2fblKWF9eNZCHttC6fRh1u1_uoOKevU",
-  authDomain: "core-schooling.firebaseapp.com",
-  projectId: "core-schooling",
-  storageBucket: "core-schooling.appspot.com",
-  messagingSenderId: "80093306721",
-  appId: "1:80093306721:web:fb6379a2c54020cee92f20"
+  apiKey: process.env.REACT_APP_apiKey, 
+  authDomain: process.env.REACT_APP_authDomain, 
+  projectId: process.env.REACT_APP_projectId, 
+  storageBucket: process.env.REACT_APP_storageBucket, 
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
